fb08559d8ad895173becdc2f928b023459adf8f6
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-04 15:24:41 +0300

    implemented task and tests

---
+++ src/arrayMethodFindIndex.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Implement method FindIndex
+ */
+function applyCustomFindIndex() {
+  [].__proto__.findIndex2 = function() {
+    // write code here
+  };
+}
+
+module.exports = applyCustomFindIndex;
